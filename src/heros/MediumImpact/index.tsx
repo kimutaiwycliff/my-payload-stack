500130c4b95af4ca88f8f2f6fec66b5019fc7f8f
Wycliff Kimutai <99463181+kimutaiwycliff@users.noreply.github.com.>
2025-07-06 21:02:41 +0300

    glowing effect

--- src/heros/MediumImpact/index.tsx
+++ src/heros/MediumImpact/index.tsx
@@ -7,6 +7,7 @@ import { Media } from '@/components/Media'
 import RichText from '@/components/RichText'
 import Bounded from '@/components/Bounded'
 import StarGrid from '@/components/StarGrid'
+import { GlowingEffect } from '@/components/ui/glowing-effect'
 
 export const MediumImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
   return (
@@ -14,35 +15,47 @@ export const MediumImpactHero: React.FC<Page['hero']> = ({ links, media, richTex
       <div className="relative">
         <StarGrid />
         <div className=" mb-8 mx-auto">
-        {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
-
-        {Array.isArray(links) && links.length > 0 && (
-          <ul className="flex gap-4 mx-auto items-center justify-center">
-            {links.map(({ link }, i) => {
-              return (
-                <li key={i}>
-                  <CMSLink {...link} />
-                </li>
-              )
-            })}
-          </ul>
-        )}
-      </div>
-      <div>
-        {media && typeof media === 'object' && (
-          <div>
-            <Media
-              className="-mx-4 md:-mx-8 2xl:-mx-16"
-              imgClassName=""
-              priority
-              resource={media}
-              pictureClassName='rounded-xl'
+          {richText && (
+            <RichText
+              className="mb-6 text-balance text-center"
+              data={richText}
+              enableGutter={false}
             />
-          </div>
-        )}
-      </div>
-      </div>
+          )}
 
+          {Array.isArray(links) && links.length > 0 && (
+            <ul className="flex gap-4 mx-auto items-center justify-center">
+              {links.map(({ link }, i) => {
+                return (
+                  <li key={i}>
+                    <CMSLink {...link} />
+                  </li>
+                )
+              })}
+            </ul>
+          )}
+        </div>
+        <div>
+          {media && typeof media === 'object' && (
+            <div className="relative">
+              <GlowingEffect
+                spread={40}
+                glow={true}
+                disabled={false}
+                proximity={64}
+                inactiveZone={0.01}
+                className='absolute -mx-4 md:-mx-8 2xl:-mx-16 rounded-xl'
+              />
+              <Media
+                className="-mx-4 md:-mx-8 2xl:-mx-16"
+                imgClassName="rounded-xl"
+                priority
+                resource={media}
+              />
+            </div>
+          )}
+        </div>
+      </div>
     </Bounded>
   )
 }

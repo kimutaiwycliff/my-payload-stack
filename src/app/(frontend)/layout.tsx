fee32c27148f42d76cba0368ba95c1076f025ea1
Wycliff Kimutai <99463181+kimutaiwycliff@users.noreply.github.com.>
2025-07-24 08:36:12 +0300

    layout issues correct

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -6,16 +6,23 @@ import { GeistSans } from 'geist/font/sans'
 import React from 'react'
 
 import { AdminBar } from '@/components/AdminBar'
+import { Header } from '@/Header/Component'
+import { Footer } from '@/Footer/Component'
 import { Providers } from '@/providers'
 import { InitTheme } from '@/providers/Theme/InitTheme'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
 import { draftMode } from 'next/headers'
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
 
 import './globals.css'
 import { getServerSideURL } from '@/utilities/getURL'
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const { isEnabled } = await draftMode()
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
 
   return (
     <html className={cn(GeistSans.variable, GeistMono.variable)} lang="en" suppressHydrationWarning>
@@ -31,8 +38,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
               preview: isEnabled,
             }}
           /> */}
-
+          {session && <Header />}
           <main>{children}</main>
+          {session && <Footer />}
         </Providers>
       </body>
     </html>
